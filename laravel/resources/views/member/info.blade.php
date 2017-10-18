9bba8971db37653ca675e6b3277bc43315fb87b5
jiayu <1214942360@qq.com>
2017-10-18 15:57:57 +0800

    路由设置-控制-视图

---
+++ laravel/resources/views/member/info.blade.php
@@ -0,0 +1,1 @@
+member-info blade view 诸如'*.blade.php'的模板文件为默认模板 参数为{{$name}} {{$age}}
--- learn/phpinfo.php
+++ learn/phpinfo.php
@@ -1,9 +1,3 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jiayu
- * Date: 2017/10/16
- * Time: 11:17
- */
 phpinfo();
 ?>
